89e7fb711efb5a017056e6421c32725a4c5ac757
Polar <rosyrdas@gmail.com>
2024-03-25 00:08:28 +0530

    refactor: remove unnecessary nesting in auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,25 +3,27 @@ import { User } from "../../db/models/index.js";
 
 export const requireAuth = async (req, res, next) => {
     const token = req.cookies.access_token;
+
     if (!token) {
-        res.status(401).send({ error: "You must be logged in" });
-    } else {
-        try {
-            const data = jwt.verify(token, process.env.JWT_SECRET);
-            if (!data?.userId) {
-                res.status(401).send({ error: "You must be logged in" });
-            } else {
-                const userInfo = await User.findById(data.userId);
-                if (!userInfo?._id) {
-                    res.status(401).send({ error: "Invalid User" });
-                } else {
-                    req.userId = userInfo._id;
-                    next();
-                }
-            }
-        } catch (e) {
-            console.error(e);
-            res.status(401).send({ error: "An error occurred" });
+        return res.status(401).json({ error: "You must be logged in" });
+    }
+
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+        if (!data?.userId) {
+            return res.status(401).json({ error: "You must be logged in" });
         }
+
+        const userInfo = await User.findById(data.userId);
+
+        if (!userInfo?._id) {
+            return res.status(401).json({ error: "Invalid User" });
+        }
+
+        req.userId = userInfo._id;
+        return next();
+    } catch (e) {
+        console.error(e);
+        return res.status(401).json({ error: "An error occurred" });
     }
 };
