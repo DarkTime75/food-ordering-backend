94c9a441f5ac1409d59dbed76369488ada49df2d
Polar <rosyrdas@gmail.com>
2024-03-25 00:01:08 +0530

    refactor: remove unnecessary template string

--- src/frameworks/utils/connectToMongoose.js
+++ src/frameworks/utils/connectToMongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoose = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URL}`);
+        const connection = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`Mongoose connected... ${connection.connection.host}`);
     } catch (error) {
         console.log("An error occured", error);
