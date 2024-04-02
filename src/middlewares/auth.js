be40c2457ebd5e85ab6b90c5c9fea2113bc6b249
Polar <rosyrdas@gmail.com>
2024-04-02 23:13:22 +0530

    fix(auth): include response

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,27 +3,31 @@ import { User } from "../../db/models/index.js";
 
 export const requireAuth = async (req, res, next) => {
     const token = req.cookies.access_token;
+    const response = { hasError: true, message: "You must be logged in", data: null };
 
     if (!token) {
-        return res.status(401).json({ error: "You must be logged in" });
+        return res.status(401).json(response);
     }
 
     try {
         const data = jwt.verify(token, process.env.JWT_SECRET);
         if (!data?.userId) {
-            return res.status(401).json({ error: "You must be logged in" });
+            response.message = "Invalid token";
+            return res.status(401).json(response);
         }
 
         const userInfo = await User.findById(data.userId);
 
-        if (!userInfo?._id) {
-            return res.status(401).json({ error: "Invalid User" });
+        if (!userInfo) {
+            response.message = "Invalid User";
+            return res.status(401).json(response);
         }
 
         req.userId = userInfo._id;
         return next();
     } catch (e) {
         console.error(e);
-        return res.status(401).json({ error: "An error occurred" });
+        response.message = "Invalid Token";
+        return res.status(401).json(response);
     }
 };
